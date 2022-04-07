b3f9ee98ba4798be6900e7a583466aa83eaaf6a7
Olivier Granoux <72553159+oliviergranoux@users.noreply.github.com>
2022-04-07 21:35:19 +0200

    update french translations

--- texts/fr.ts
+++ texts/fr.ts
@@ -1,24 +1,24 @@
 export default {
   siteName: `Stand For Ukraine`,
-  siteDescription: `Soutenir l'Ukraine face à l'agression militaire Russe. Faire un don à des projets vérifiés afin d'aider les Forces Armées Ukrainiennes ou l'aide humanitaire. Informer le public en partageant nos bulletins d'informations. `,
+  siteDescription: `Soutenir l'Ukraine face à l'agression militaire russe. Faire un don à des projets vérifiés afin d'aider les Forces Armées Ukrainiennes ou l'aide humanitaire. Informer le public en partageant nos bulletins d'informations. `,
   thumbnail: `/thumbnail.png`,
 
-  share: 'Partage',
-  donate: 'Dons',
-  inform: 'Informations',
-  donateButton: 'Dons',
+  share: 'Partager',
+  donate: 'Donner',
+  inform: 'Informer',
+  donateButton: 'Donner',
 
   heroHeader: `Aider les militaires ukrainiens et les organisations humanitaires`,
-  heroText1: `Soutenir l'Ukraine face à l'agression militaire Russe.`,
-  heroText2: `Dons à des projets vérifiés afin d'aider les Forces Armées Ukrainiennes ou l'aide humanitaire.`,
+  heroText1: `Soutenir l'Ukraine face à l'agression militaire russe.`,
+  heroText2: `Donner à des projets vérifiés afin d'aider les Forces armées ukrainiennes ou l'aide humanitaire.`,
   heroText3: `Informer le public en partageant nos bulletins d'informations.`,
 
   close: 'Fermer',
   sharePopupTitle: 'Faire circuler la cause',
-  sharePopupText1: 'Partager la ressource à vos amis et collègues.',
+  sharePopupText1: 'Partager cette ressource avec vos amis et collègues.',
   sharePopupText2: "Faire savoir aux gens comment soutenir l'Ukraine.",
   sharingText:
-    "Aider les militaires ukrainiens et les organisations humanitaires #Soutien à l'Ukraine. ",
+    "Aider les militaires ukrainiens et les organisations humanitaires #StandForUkraine. ",
   copyLink: 'Copier le lien',
   copyLinkDone: 'Copié!',
 
@@ -30,31 +30,31 @@ export default {
   Refugees: 'Réfugiés',
   Press: 'Presse',
   Veterancy: 'Vétérans',
-  'Non-lethal': 'Non mortel',
+  'Non-lethal': 'Non létal',
   'Non-government': 'Non gouvernemental',
-  'Human Rights': 'Droits humains',
+  'Human Rights': 'Droits de l’homme',
 
   // footer
-  footerHeader: `Soutien à l'Ukraine : c'est le rassemblement d'organisations fiables qui aident à combattre l'invasion russe et à faire face à ses conséquences.`,
+  footerHeader: `Stand For Ukraine, c'est le rassemblement d'organisations fiables qui aident à combattre l'invasion russe et à faire face à ses conséquences.`,
   footerGoals: 'Nos objectifs',
   goal1: 'Aider les militaires ukrainiens avec des armes, des munitions et des fournitures',
-  goal2: 'Rétablissement des vétérans et des victimes de guerre',
+  goal2: 'Aider les vétérans et les victimes de guerre à se rétablir',
   goal3: 'Abriter et nourrir les réfugiés',
   goal4: 'Aider les enfants, les aînés et les personnes vulnérables',
   goal5: 'Soutenir la presse indépendante',
-  footerCreds: `Ce site Web a été créé par des ingénieurs, managers, désigners, écrivains, traducteurs, journalistes et activistes de toute l'Ukraine et d'ailleur.`,
-  footerContact: 'Rejoins-nous',
+  footerCreds: `Ce site Web a été créé par des ingénieurs, managers, désigners, écrivains, traducteurs, journalistes et activistes de toute l'Ukraine et d'ailleurs.`,
+  footerContact: 'Contactez-nous',
   joinUs: 'Rejoignez-nous',
-  disclaimler: 'Nous n\'acceptons pas de paiement; nous fournissons seulement les liens directs vers les pages de dons.',
+  disclaimler: 'Nous n’acceptons pas de paiement; nous ne fournissons que des liens directs vers des pages de dons.',
   aboutProject: 'À propos du projet',
   suggestOrgLink: 'Suggérer une association',
   sharedFeedbackLink: 'Partager des commentaires',
   joinFormLink: 'Rejoindre la légion internationale',
 
   // filter
-  filterTo: 'Associations',
+  filterTo: 'Organismes',
   filterPayVia: 'Méthodes de paiement',
-  resetFilter: 'Réinitialiser',
+  resetFilter: 'Réinitialiser le filtre',
 
   // payment methods
   IBAN: 'IBAN',
@@ -65,8 +65,8 @@ export default {
   'Western Union': 'Western Union',
   Kuna: 'Kuna',
 
-  copyCode: 'Copier le code',
-  browseAll1: 'Parcourir',
+  copyCode: 'Copier le code', /*used in EDRPOU popup*/
+  browseAll1: 'Parcourir les',
   browseAll2: 'associations',
 
   // about page
@@ -114,8 +114,8 @@ export default {
 
   // legal popup
   legalCodeLabel: 'ЄДРПОУ',
-  legalDesc1: '\"ЄДРПОУ" (EDRPOU) est le code de l\'entité juridique du Service national des impôts ukrainien.',
+  legalDesc1: '\"ЄДРПОУ" (EDRPOU) est le code de l’entité juridique du Service national des impôts ukrainien.',
   legalDesc2: 'Copier le code EDRPOU',
-  legalDesc3: 'Vérifier l\'association grâce au code dans le registre.',
+  legalDesc3: 'Vérifier l’association grâce au code dans le registre',
   legalFooterLink: 'Service national des impôts ukrainien',
 }
